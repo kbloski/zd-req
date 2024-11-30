fc62f79fe955d007291ad84d136323702e56da7b
Kamil Błoński <kblonski02@gmail.com>
2024-11-30 12:23:04 +0100

    update

--- frontend/js/paralaks_dog_background.js
+++ frontend/js/paralaks_dog_background.js
@@ -1,12 +1,15 @@
-const backgroundItems = document.getElementById( "dog-baner-paralax-elements");
-const n = 0.5
+const backgroundItems = document.getElementById("dog-baner-paralax-elements");
+const n = 0.5;
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
     if (!backgroundItems) return;
 
-    const backgroundItemsPos = backgroundItems.getBoundingClientRect()
+    const backgroundItemsPos = backgroundItems.getBoundingClientRect();
+    if (-1 * backgroundItemsPos.top > backgroundItems.scrollHeight) return;
 
-    if (backgroundItemsPos.top - window.screen.availHeight/2 < 0){
-        backgroundItems.style.transform = `translateY(${-( n *backgroundItemsPos.top )}px)`
+    if (backgroundItemsPos.top - window.screen.availHeight / 2 < 0) {
+        backgroundItems.style.transform = `translateY(${-(
+            n * backgroundItemsPos.top
+        )}px)`;
     }
-})
+});
